c9354c0440d2f678c2f6fe0a5b3a284c28d6b5bb
rahul <rahul.lohakare@varian.com>
2018-12-23 19:59:36 +0530

    updated modal to statefull component

--- src/UI/Modal/modal.js
+++ src/UI/Modal/modal.js
@@ -3,13 +3,21 @@ import classes from "./modal.css";
 import BackDrop from "../BackDrop/backDrop";
 import Aux from '../../hoc/auxilary';
 
-const Modal = (props) => (
-    <Aux>
-        <BackDrop clicked={props.cancelPurchasing} show={props.showBackDrop}/>
-        <div style={{transform : props.show ? "translateY(0)" : "translateY(-100vh)"}} className={classes.Modal}>
-            {props.children}
-        </div>
-    </Aux>
-)
+class Modal extends Component {
+    shouldComponentUpdate(newProps , newState) {
+        return newProps.show !== this.props.show
+    }
+
+    render () {
+        return (
+            <Aux>
+                <BackDrop clicked={this.props.cancelPurchasing} show={this.props.showBackDrop}/>
+                <div style={{transform : this.props.show ? "translateY(0)" : "translateY(-100vh)"}} className={classes.Modal}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+}
 
 export default Modal;
